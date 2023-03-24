f604b6e109817e32400e58bf85affcbde2f3fe6f
IvanBichan <ivanbichanfe@gmail.com>
2023-03-24 16:48:42 +0300

    add player 1 img and animate

--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,11 @@
 class Sprite {
-    constructor({position, imageSrc,scale= 1, framesMax = 1}) {
+    constructor({
+                    position,
+                    imageSrc,
+                    scale = 1,
+                    framesMax = 1,
+                    offset = {x: 0, y: 0}
+                }) {
         this.position = position
         this.width = 50
         this.height = 150
@@ -10,45 +16,61 @@ class Sprite {
         this.frameCurrent = 0
         this.frameElapsed = 0
         this.framesHold = 5
+        this.offset = offset
     }
 
     draw() {
         c.drawImage(
             this.image,
-            this.frameCurrent * (this.image.width/ this.framesMax),
+            this.frameCurrent * (this.image.width / this.framesMax),
             0,
-            this.image.width/this.framesMax,
+            this.image.width / this.framesMax,
             this.image.height,
-            this.position.x,
-            this.position.y,
-            (this.image.width/this.framesMax) * this.scale,
+            this.position.x - this.offset.x,
+            this.position.y - this.offset.y,
+            (this.image.width / this.framesMax) * this.scale,
             this.image.height * this.scale,
         )
     }
 
-    update() {
-        this.draw()
+    animateFrames() {
         this.frameElapsed++
-        if(this.frameElapsed%this.framesHold === 0) {
-            if(this.frameCurrent<this.framesMax - 1) {
+        if (this.frameElapsed % this.framesHold === 0) {
+            if (this.frameCurrent < this.framesMax - 1) {
                 this.frameCurrent++
             } else {
                 this.frameCurrent = 0
             }
         }
+    }
 
-
+    update() {
+        this.draw()
+        this.animateFrames()
     }
 }
 
-class Fighter {
-    constructor({position, velocity, color = 'red', offset}) {
-        this.position = position
+class Fighter extends Sprite {
+    constructor({
+                    position,
+                    velocity,
+                    color = 'red',
+                    imageSrc,
+                    scale = 1,
+                    framesMax = 1,
+                    offset = {x: 0, y: 0},
+                }) {
+        super({
+            position,
+            imageSrc,
+            scale,
+            framesMax,
+            offset
+        })
         this.velocity = velocity
         this.width = 50
         this.height = 150
-        this.lastKey = undefined
-        this.health = 100
+        this.lastKey
         this.attackBox = {
             position: {
                 x: this.position.x,
@@ -60,9 +82,13 @@ class Fighter {
         }
         this.color = color
         this.isAttacking = undefined
+        this.health = 100
+        this.frameCurrent = 0
+        this.frameElapsed = 0
+        this.framesHold = 5
     }
 
-    draw() {
+    /*draw() {
         c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
         //attack box
@@ -75,16 +101,18 @@ class Fighter {
                 this.attackBox.height
             )
         }
-    }
+    }*/
 
     update() {
         this.draw()
+        this.animateFrames()
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity.y >= canvas.height-97) {
+        if (this.position.y + this.height + this.velocity.y >= canvas.height - 97) {
             this.velocity.y = 0
         } else {
             this.velocity.y += gravity
