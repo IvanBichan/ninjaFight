80944f56158874ae6c656ddf8fb623740bf5e208
IvanBichan <ivanbichanfe@gmail.com>
2023-04-06 15:13:14 +0300

    feat: add to jump animation for payer

--- js/classes.js
+++ js/classes.js
@@ -89,11 +89,10 @@ class Fighter extends Sprite {
         this.framesHold = 5
         this.sprites = sprites
 
-        for(const sprite in this.sprites) {
+        for (const sprite in this.sprites) {
             sprites[sprite].image = new Image()
             sprites[sprite].image.src = sprites[sprite].imageSrc
         }
-        console.log(this.sprites)
     }
 
     /*draw() {
@@ -119,9 +118,10 @@ class Fighter extends Sprite {
         this.attackBox.position.y = this.position.y
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-
+//graviti function
         if (this.position.y + this.height + this.velocity.y >= canvas.height - 97) {
             this.velocity.y = 0
+            this.position.y = 330
         } else {
             this.velocity.y += gravity
         }
@@ -132,5 +132,38 @@ class Fighter extends Sprite {
         setTimeout(() => {
             this.isAttacking = false
         }, 100)
+    }
+
+    switchSprite(sprite) {
+        switch (sprite) {
+            case 'idle':
+                if (this.image !== this.sprites.idle.image) {
+                    this.image = this.sprites.idle.image
+                    this.framesMax = this.sprites.idle.framesMax
+                    this.frameCurrent = 0
+                }
+                break;
+            case 'run':
+                if (this.image !== this.sprites.run.image) {
+                    this.image = this.sprites.run.image
+                    this.framesMax = this.sprites.run.framesMax
+                    this.frameCurrent = 0
+                }
+                break;
+            case 'jump':
+                if (this.image !== this.sprites.jump.image) {
+                    this.image = this.sprites.jump.image
+                    this.framesMax = this.sprites.jump.framesMax
+                    this.frameCurrent = 0
+                }
+                break;
+                case 'fall':
+                if (this.image !== this.sprites.fall.image) {
+                    this.image = this.sprites.fall.image
+                    this.framesMax = this.sprites.fall.framesMax
+                    this.frameCurrent = 0
+                }
+                break;
+        }
     }
 }
